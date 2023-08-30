a6131bc1a68d4039485450aec2d9c4f5ab4b6168
macupon <macupon@gmail.com>
2023-08-30 12:14:55 +0200

    footer

--- src/components/aboutUs/AboutUs.js
+++ src/components/aboutUs/AboutUs.js
@@ -2,7 +2,9 @@
                     IMPORTACIONES
 ######################################################*/
 // Modulos
-
+import { Helmet } from 'react-helmet';
+import React from 'react';
+import mariImg from '../../assets/img/mariProcreate.png';
 // Estilos
 import './AboutUs.css'
 
@@ -16,10 +18,19 @@ import './AboutUs.css'
 const  AboutUs = () => { //Funcion constructora
     // retorno que se va a randerizar
     return (
-       <p>
-        AboutUs
-    
-       </p>
+        <div className='nosotros-div'>
+            <Helmet>
+                <style>{'body{ background-color: #F8CDDD;}'}</style>
+            </Helmet>
+            <div className='nosotros-img-text'>
+                <div>
+                    <img className='nosotros-img' src={mariImg}  alt=""></img>
+                </div>
+                <p className='nosotros-text-div'>
+                    Hola!!! Soy Marianita <br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+        </div>
     )
 }
 
@@ -27,4 +38,4 @@ const  AboutUs = () => { //Funcion constructora
 /* ####################################################
                     EXPORTACIONES
 ######################################################*/
-export default AboutUs
+export default AboutUs;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -13,10 +13,10 @@ import { FaInstagram } from "react-icons/fa";
 const  Footer = () => { //Funcion constructora
     // retorno que se va a randerizar
     return (
-       <p className='foot social'>
+       <div className='foot'>
         <a href='https://www.facebook.com/apliquesmarian' ><FaFacebookSquare size={35}/></a>
         <a href='https://www.instagram.com/apliquesmarian/' ><FaInstagram size={35}/></a>
-       </p>
+       </div>
     )
 }
 
